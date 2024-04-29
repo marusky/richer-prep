20011bd767ef9aa4c6914871ffddf2ccbeb961d1
marusky <adrianmarusakza@gmail.com>
2024-04-29 10:59:01 +0200

    Add registration and login

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'up', to: 'rails/health#show', as: :rails_health_check
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get 'secret', to: 'pages#secret'
 
-  # Defines the root path route ("/")
+  resources :users, only: %i[index destroy]
+  get 'register', to: 'users#new'
+  post 'register', to: 'users#create'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
   root 'pages#home'
 end
