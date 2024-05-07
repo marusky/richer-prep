514977c5ba39ac9fed0287dfb8d76c73825290f7
Adrián Marušák <adrianmarusakza@gmail.com>
2024-05-07 16:07:00 +0200

    Revert "Add transactions (#3)"
    This reverts commit 357d67d13aba6306391f021c6ef8ab2874a6cc27.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,2 @@
 module ApplicationHelper
-  def active_for(path: nil, controller: nil)
-    if path && path == request.path ||
-         controller && controller == controller_name
-      'active'
-    end
-  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_30_140328) do
-  create_table "transactions", force: :cascade do |t|
-    t.string "title"
-    t.integer "kind", default: 0, null: false
-    t.float "amount", default: 0.0, null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
+ActiveRecord::Schema[7.1].define(version: 2024_04_29_123405) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
