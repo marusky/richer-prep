e2e5ff0b9617e7a1ccf09a0be387f1d7bd9f57cf
marusky <adrianmarusakza@gmail.com>
2024-06-20 14:37:39 +0200

    Add rspec (main)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
+  validates :given_name, :family_name, presence: true
   has_many :transactions
   has_many :categories
 
   devise :database_authenticatable, :registerable, :validatable
+
+  def full_name
+    "#{given_name} #{family_name}"
+  end
 end
